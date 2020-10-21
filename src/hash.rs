60af1e866043effce2542d0af9b453dfb6fa1674
Jules de Smit <julesdesmit@gmail.com>
2020-10-21 18:46:08 +0200

    Opt for Blake2b to avoid clamping
    This allows us to use the infallible `from_bytes_wide`
    function, which removes the need to do a hacky
    modification to the hash output in order to
    guarantee success.

--- src/hash.rs
+++ src/hash.rs
@@ -7,34 +7,16 @@
 //! Defines the hash functions needed for the BLS signature scheme.
 
 use crate::PublicKey;
-use blake2::{Blake2s, Digest};
+use blake2::{Blake2b, Digest};
 use dusk_bls12_381::{G1Affine, Scalar as BlsScalar};
 
 /// h0 is the hash-to-curve-point function.
 /// Hₒ : M -> Gₒ
 pub fn h0(msg: &[u8]) -> G1Affine {
-    // Hash the given message.
-    let mut hasher = Blake2s::new();
-    hasher.update(msg);
-    let res = hasher.finalize();
-
-    let mut arr = [0u8; 32];
-    arr.copy_from_slice(&res[..]);
-
-    // Ensure that the value is lower than the BLS12-381
-    // modulus, by cutting the value to be max 2^254.
-    // This will create a canonical array of bytes which
-    // we can convert into a BLS scalar without issues.
-    arr[31] &= 0b0011_1111;
-
-    // Turn the hash into a BLS scalar.
-    // NOTE: the unwrap here is done for a reason: it is
-    // mimicking the code found here:
-    // https://github.com/dusk-network/bls12_381/blob/master/src/scalar.rs#L473
-    //
-    // We should be able to unwrap safely since we cut the
-    // value to be < 2^254.
-    let scalar = BlsScalar::from_bytes(&arr).unwrap();
+    let hash = Blake2b::digest(msg);
+    let mut arr = [0u8; 64];
+    arr.copy_from_slice(&hash[..]);
+    let scalar = BlsScalar::from_bytes_wide(&arr);
 
     // Now multiply this message by the G1 base point,
     // to generate a G1Affine.
@@ -46,15 +28,9 @@ pub fn h0(msg: &[u8]) -> G1Affine {
 /// multi-signature construction.
 /// H₁: G₂ -> R
 pub fn h1(pk: &PublicKey) -> BlsScalar {
-    let mut hasher = Blake2s::new();
-    hasher.update(pk.to_bytes());
-    let res = hasher.finalize();
-
-    let mut arr = [0u8; 32];
-    arr.copy_from_slice(&res[..]);
-
-    // NOTE: see above explanation
-    arr[31] &= 0b0011_1111;
-    let scalar = BlsScalar::from_bytes(&arr).unwrap();
+    let hash = Blake2b::digest(&pk.to_bytes());
+    let mut arr = [0u8; 64];
+    arr.copy_from_slice(&hash[..]);
+    let scalar = BlsScalar::from_bytes_wide(&arr);
     scalar
 }
