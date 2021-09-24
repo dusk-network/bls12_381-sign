beb1fb00c2ea02fc40facff1929c2aef603dd3a2
Loki <david@dusk.network>
2021-09-24 14:54:20 +0300

    this doesn't work, just to put it up

--- src/svc/svc.rs
+++ src/svc/svc.rs
@@ -11,8 +11,10 @@ use std::path::Path;
 use futures::TryFutureExt;
 #[cfg(unix)]
 use tokio::net::UnixListener;
-use tonic::{transport::Server, Request, Response, Status};
+use tonic::{transport::Server, IntoRequest, Request, Response, Status};
 
+use crate::signer::sign_response;
+use bls12381sig::{Error, PublicKey, SecretKey, Signature, APK};
 use signer::{
     signer_server::{Signer, SignerServer},
     AggregatePkRequest, AggregateResponse, AggregateSigRequest,
@@ -27,29 +29,52 @@ pub struct MySign {}
 
 #[tonic::async_trait]
 impl Signer for MySign {
+    // BLS12-381 Signer service implementation
+
+    // Generate a new BLS12-381 key pair
     async fn generate_keys(
         &self,
-        request: Request<()>,
+        _request: Request<()>,
     ) -> Result<Response<GenerateKeysResponse>, Status> {
-        let reply = GenerateKeysResponse {
-            private_key: vec![],
-            public_key: vec![],
-        };
-        Ok(Response::new(reply))
+        // get a new random secret key from system entropy
+        let sk = SecretKey::new(&mut rand_core::OsRng);
+        // construct the gRPC response from the key and return the response
+        Ok(Response::new(GenerateKeysResponse {
+            private_key: sk.to_bytes().to_vec(),
+            public_key: PublicKey::from(&sk).to_bytes().to_vec(),
+        }))
     }
 
+    // Sign a message using a provided BLS12-381 key pair
     async fn sign(
         &self,
         request: Request<SignRequest>,
     ) -> Result<Response<SignResponse>, Status> {
-        let reply = SignResponse { response: None };
-        Ok(Response::new(reply))
+        // access the request parameters
+        let req: SignRequest = request.into_request().into_inner();
+        // read in the secret key
+        let sk = req.private_key;
+        // read in the public key
+        let pk = req.public_key.as_slice();
+        // read in the message to be signed
+        let msg = req.message.as_slice();
+
+        let mut sk = SecretKey::from_bytes(sk.to_vec().)
+        // let pk = PublicKey::from_bytes(req.public_key.to_bytes());
+        // let msg = req.message;
+        // sign the message
+        // let sig = SignResponse {
+        // response: sk.sign(&pk, &msg).to_bytes(),
+        // };
+        Ok(Response::new(SignResponse { response: None }))
     }
 
     async fn verify(
         &self,
         request: Request<VerifyRequest>,
     ) -> Result<Response<VerifyResponse>, Status> {
+        // let req = request.into_request().into_inner();
+
         let reply = VerifyResponse { response: None };
         Ok(Response::new(reply))
     }
