e3d747ec5e5144748ca77d752956776f79167dc6
Loki <david@dusk.network>
2021-09-25 14:43:57 +0300

    added aggregate_pk implementation

--- src/svc/svc.rs
+++ src/svc/svc.rs
@@ -13,8 +13,10 @@ use futures::TryFutureExt;
 use tokio::net::UnixListener;
 use tonic::{transport::Server, IntoRequest, Request, Response, Status};
 
+use crate::signer::create_apk_response::Apk::Apk;
 use dusk_bls12_381_sign::{Error, PublicKey, SecretKey, Signature, APK};
 use signer::{
+    aggregate_response::Agg,
     sign_response::Sig,
     signer_server::{Signer, SignerServer},
     verify_response::Ver,
@@ -23,7 +25,6 @@ use signer::{
     SignResponse, VerifyRequest, VerifyResponse,
 };
 use std::convert::{TryFrom, TryInto};
-use crate::signer::create_apk_response::Apk::Apk;
 
 mod signer;
 
@@ -39,7 +40,6 @@ impl Signer for MySign {
         &self,
         _request: Request<()>,
     ) -> Result<Response<GenerateKeysResponse>, Status> {
-
         // get a new random secret key from system entropy
         let sk = SecretKey::new(&mut rand_core::OsRng);
 
@@ -55,7 +55,6 @@ impl Signer for MySign {
         &self,
         request: Request<SignRequest>,
     ) -> Result<Response<SignResponse>, Status> {
-
         // access the request parameters
         let req = request.get_ref();
 
@@ -95,9 +94,7 @@ impl Signer for MySign {
 
         // sign the message
         let res = Sig::Signature(
-            sk.sign(&pk, req.message.as_slice())
-                .to_bytes()
-                .to_vec(),
+            sk.sign(&pk, req.message.as_slice()).to_bytes().to_vec(),
         );
 
         // return the signature wrapped in the response type
@@ -110,7 +107,6 @@ impl Signer for MySign {
         &self,
         request: Request<VerifyRequest>,
     ) -> Result<Response<VerifyResponse>, Status> {
-
         // access the request parameters
         let req = request.get_ref();
 
@@ -165,7 +161,6 @@ impl Signer for MySign {
         &self,
         request: Request<CreateApkRequest>,
     ) -> Result<Response<CreateApkResponse>, Status> {
-
         // access the request parameters
         let req = request.get_ref();
 
@@ -189,7 +184,7 @@ impl Signer for MySign {
         // convert public key to aggregated public key and return it
         let apk = APK::from(&pk);
         Ok(Response::new(CreateApkResponse {
-            apk: Some(Apk(apk.to_vec())),
+            apk: Some(Apk(apk.to_bytes().to_vec())),
         }))
     }
 
@@ -198,10 +193,62 @@ impl Signer for MySign {
         request: Request<AggregatePkRequest>,
     ) -> Result<Response<AggregateResponse>, Status> {
 
+        // access the request parameters
+        let req = request.get_ref();
 
+        // check length of public key and convert to fixed length array
+        let apk =
+            <&[u8; PublicKey::serialized_size()]>::try_from(req.apk.as_slice());
+        if apk.is_err() {
+            return Err(Status::invalid_argument(
+                "provided (aggregated) public key is wrong length",
+            ));
+        }
+        let apk = apk.unwrap();
 
-        let reply = AggregateResponse { agg: None };
-        Ok(Response::new(reply))
+        // create new aggregated public key from provided bytes
+        let apk = APK::from_bytes(apk);
+        if apk.is_err() {
+            return Err(Status::invalid_argument(
+                "(aggregated) public key failed to decode",
+            ));
+        }
+        let mut apk = apk.unwrap();
+
+        // convert the raw bytes from the message to a collection of public keys
+        let mut pks: Vec<PublicKey> = Vec::with_capacity(req.keys.len());
+        for (i, key) in req.keys.iter().enumerate() {
+
+            // check the length of the public key and convert to fixed length array
+            let pk =
+                <&[u8; PublicKey::serialized_size()]>::try_from(key.as_slice());
+            if pk.is_err() {
+                return Err(Status::invalid_argument(
+                    "provided public key is wrong length",
+                ));
+            }
+            let pk = pk.unwrap();
+            // create a new public key from the provided bytes
+            let pk = PublicKey::from_bytes(pk);
+            if pk.is_err() {
+                return Err(Status::invalid_argument(
+                    "error decoding public key",
+                ));
+            }
+            let pk = pk.unwrap();
+
+            // append to collection of PublicKeys
+            pks[i] = pk;
+        }
+
+        // aggregate the keys
+        apk.aggregate(&pks.as_slice());
+        // convert public key to aggregated public key and return it
+        let apb = apk.to_bytes();
+        Ok(Response::new(AggregateResponse {
+            // agg: Some(Agg::from(apb.into())),
+            agg: None,
+        }))
     }
 
     async fn aggregate_sig(
