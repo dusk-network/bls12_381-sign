cac2680a693832da7cb993a2a6d24a86b5772465
Loki <david@dusk.network>
2021-09-26 22:22:36 +0300

    switched to simplified in-build generation

--- build.rs
+++ build.rs
@@ -1,13 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // compiling protos using path on build time
-    tonic_build::configure()
-        .build_client(true)
-        .build_server(true)
-        .out_dir("src/svc") // you can change the generated code's location
-        .compile(
-            &["./proto/bls12381sig.proto"],
-            &["./proto"], // specify the root location to search proto dependencies
-        )
-        .unwrap();
+    tonic_build::compile_protos("./proto/bls12381sig.proto")?;
     Ok(())
 }
