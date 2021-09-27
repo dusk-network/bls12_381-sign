eac5d98d274f1f41ce2fad8399a42b8712371629
Loki <david@dusk.network>
2021-09-27 12:20:26 +0300

    added parameters to clap spec for commands

--- src/svc/client.rs
+++ src/svc/client.rs
@@ -7,7 +7,7 @@
 #![cfg_attr(not(unix), allow(unused_imports))]
 
 use clap::App;
-use signer::{signer_client::SignerClient, GenerateKeysRequest};
+// use signer::{signer_client::SignerClient, GenerateKeysRequest};
 
 pub mod signer;
 
@@ -18,25 +18,41 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .version("v0.1.0")
         .author("Loki <david@dusk.network>")
         .about("Generate keys, sign, aggregate and verify messages using BLS12-381 elliptic curves")
-        .subcommand(App::new("generatekeys")
-            .about("generate a secret and public key pair"))
+        .subcommand(App::new("gen")
+            .about("Generate a new BLS12-381 key pair"))
         .subcommand(App::new("sign")
-            .about("sign a message with a given key pair"))
+            .about("Sign a message with a given key pair")
+            .arg("<secretkey> 'Secret key'")
+            .arg("<publickey> 'Public key'")
+            .arg("<message> 'Message to be signed'")
+        )
         .subcommand(App::new("verify")
-            .about("Verify a signature against message and public key"))
+            .about("Verify a signature against message and public key")
+            .arg("<apk> '(Aggregated) Public key'")
+            .arg("<signature> 'Signature'")
+            .arg("<message> 'Message bearing signature'")
+        )
         .subcommand(App::new("createapk")
-            .about("Convert a public key into an aggregated public key"))
+            .about("Convert a public key into an aggregated public key")
+            .arg("<publickey> 'Public key'")
+       )
         .subcommand(App::new("aggregatepk")
-            .about("add more public keys ot an aggregated public key"))
+            .about("Add more public keys to an aggregated public key")
+            .arg("<secretkey> 'Aggregated public key'")
+            .arg("<publickey>... 'Public Key'")
+       )
         .subcommand(App::new("aggregatesig")
-            .about("aggregate multiple signatures into one signature"))
+            .about("Aggregate multiple signatures into one signature")
+            .arg("<signature> 'signature to aggregate into'")
+            .arg("<signatures>... 'signatures to be merged into the first'")
+      )
         .get_matches();
-    // creating a channel ie connection to server
-    let channel =
-        tonic::transport::Channel::from_static("http://127.0.0.1:9156")
-            .connect()
-            .await?;
-    let mut client = SignerClient::new(channel);
-
+    // // creating a channel ie connection to server
+    // let channel =
+    //     tonic::transport::Channel::from_static("http://127.0.0.1:9156")
+    //         .connect()
+    //         .await?;
+    // let mut client = SignerClient::new(channel);
+    println!("{:?}", matches);
     Ok(())
 }
